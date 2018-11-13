009ce3a2150bc8de013a284d53780e4b4a18167a
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2018-11-13 15:11:44 +0200

    bootstrap initial data

---
+++ src/main/java/alex/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package alex.springfamework.bootstrap;
+
+import alex.springfamework.domain.Category;
+import alex.springfamework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Bootstrap implements CommandLineRunner {
+
+    public CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Data loaded = " + categoryRepository.count());
+
+    }
+}
